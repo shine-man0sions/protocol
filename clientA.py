f1210c6e308aa8c9287fa91bb59502512a6273b4
v_sttsun <v_sttsun@tencent.com>
2023-04-21 13:37:32 +0100

    change the commit for client

--- clientA.py
+++ clientA.py
@@ -2,7 +2,10 @@ from client_new import ChatClient
 
 import socket
 
-"""Press the green button in the gutter to run the script."""
+"""
+    :param host, port, client
+    return a instance of class Client
+"""
 if __name__ == '__main__':
     ChatClient = ChatClient(host="127.0.0.1", port=5006, client="A")
     ChatClient.start_client()
