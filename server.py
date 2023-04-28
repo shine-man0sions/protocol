83754afb0d304b9fb23bbfe6b232b1edda7d0523
v_sttsun <v_sttsun@tencent.com>
2023-04-28 09:50:13 +0100

    完整

--- server.py
+++ server.py
@@ -1,216 +1,172 @@
-import datetime
 import socket
 import select
-import sys
+import queue
 import pickle
 from utils import *
-import secrets
-import hmac
-from Crypto.Util.number import bytes_to_long, long_to_bytes
-from Crypto.Random import get_random_bytes
 
 
-class ChatClient:
-    def __init__(self, config_file="config.json", host="127.0.0.1", port=5006, client=""):
+class ChatServer():
+    def __init__(self, config_file="config.json", host="127.0.0.1", port=5006):
         self.host = host
         self.port = port
-        self.client = client
+        self.server_socket = None
         self.all_ca_dict = read_json_to_dict(config_file)
         self.all_public_ca_dict = self.all_ca_dict.get("public_key")
-        self.public_ca_name = f"{client}_CA_public_key"
-        self.ca_name = f"{client}_CA_key"
-        self.client_public_key = self.all_ca_dict.get("public_key").get(self.public_ca_name)
+        self.sock_dict = {}
+        self.clients = {}
+        self.sock_reply_msg_dict = {}
+        self.public_ca_name = "S_CA_public_key"
+        self.ca_name = "S_CA_key"
+        self.client_public_key = self.all_ca_dict.get(
+            "public_key").get(self.public_ca_name)
         self.client_key = self.all_ca_dict.get("key").get(self.ca_name)
-        self.public_cas = self.all_ca_dict.get("public_key").get("S_CA_public_key")
-        self.sign_name = f"{client}_CA_key_sign"
-        self.sign_key = self.all_ca_dict.get("key_sign").get(self.sign_name)
-        self.Kabc = None
-        self.key_dict = {
-            self.client: generate_random_hash256()
-        }
-        self.key_dict_all = {
-        }
-        self.key_dict_temporary = {
-            self.client: generate_random_hash256()[:32]
-        }
 
-    """
-        :param: sock
-        :return: 1. login this is the first step, each client need to login to server to make sure 
-                 the client is valid and they can communicate with S server
-                 2. this part include authentication and integrity authentication: 
-                 3. use rsa to encrypt the client public key and send to server 
-                 4. integrity: use rsa to signature client public key and send to server
-    """
+    def server(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((self.host, self.port))
+        server_socket.listen(5)
+        print('Server is running on {}:{}'.format(*server_socket.getsockname()))
+        self.server_socket = server_socket
+        inputs = [server_socket]
+        outputs = []
+        message_queues = {}
 
-    def login(self, sock):
-        login_message = {
-            "action": "login",
-            "content": {
-                "public_ca_name": f"{self.client}_CA_public_key",
-                "public_ca_value": self.client_public_key,
-                "sign_name": f"{self.client}_sign_key",
-                "sign_value": self.sign_key,
-                "send_source": self.client
-            }
-        }
-        text = {
-            "client": self.client,
-            "text": f"{self.client}_CA_public_key"
-        }
-        message = transmit_encrypt_func(text, self.client_key, self.public_cas, login_message)
-
-        sock.sendall(message)
-
-        # Receive server response data, if login success,
-        # print response message, if failure, print failure message reminder
-        login_res_dict = pickle.loads(sock.recv(RECV_LEN))
-
-        reply_action = login_res_dict.get("reply_action")
-        if reply_action == "login_success":
-            print(f"step 1 {self.client} can connect to S => {login_res_dict.get('reply_action')}")
-        return None
-
-    def exchange_message(self, sock, send_to1):
-
-        # Obtain random numbers Na, Nb, Nc, and then forward them to other clients
-        key_random = self.key_dict.get(self.client)
-        self.handle_send_msg(sock, send_to1, "", key_random)
-
-        return None
-
-    """
-        :param: sock, send_to, message, hash
-        :return: Entity A, B, C send message to Entity A, B, C 
-                 case 1: Entity authenticated itself to A and B, B 
-                 case 2: integrity of data
-    """
-
-    def handle_send_msg(self, sock, send_to, message, hash):
-        public_key = self.all_ca_dict.get("public_key").get(f"{send_to}_CA_public_key")
-        new_message = message
-        if self.Kabc:
-            new_message = aes_encrypt(self.Kabc, message)
-
-        send_msg = {
-            "action": "change_message",
-            "content": {
-                "send_source": self.client,
-                "send_to": send_to,
-                "msg": new_message,
-                "hash": hash,
-            }
-        }
-        optional = send_msg["content"]
-        text = {
-            "client": self.client,
-            "random": self.key_dict.get(f"{self.client}"),
-            "send_to": send_to,
-        }
-        print(f"Server S sent message to Client =====>> {text}")
-
-        message = transmit_encrypt_func(text, self.client_key, public_key, send_msg, optional)
-        sock.sendall(message)
-        return None
-
-    def receive_message(self, conn, result):
-        try:
-            if result == "":
-                return
-
-            if isinstance(result, dict) == False:
-                printMsg("==> receive from server ", result)
-                return
-            message = result["message"]
-            # The private key of the S server is used to decrypt the client using the rsa decryption algorithm
-            dec_text = rsa_decrypt(self.client_key, message["key"])
-            plain_text = bytes_to_dict(dec_text)
-            print(f"{plain_text}")
-            client = plain_text["client"]
-
-            # Construct message data that is used to verify digital signatures
-            hash_result = {"hashKey": double_hash(plain_text)}
-            hash_result_s = message["sign"]
-
-            # Obtain the client's public key, which is used to verify the digital signature
-            public_key = self.all_ca_dict.get("public_key").get(f"{client}_CA_public_key")
-
-            # Verify digital signatures. compare True indicates that the verification succeeds,
-            # False indicates that the verification fails
-            compare = RSA_verify(public_key, hash_result, hash_result_s)
-
-            if compare:
-                cipher = bytes_to_dict(message["cipher"])
-                content = cipher["content"]
-                msg = content["msg"]
-                hash = content["hash"]
-                send_source = content["send_source"]
-                if hash:
-                    if "===" in hash:
-                        self.key_dict_all[send_source] = hash
-                    else:
-                        self.key_dict[send_source] = hash
-                        new_msg = hash + "===" + send_source
-                        self.handle_send_msg(conn, send_source, "", new_msg)
-                        printMsg("==> self.key_dict ", self.key_dict)
+        while inputs:
+            readables, writables, exceptions = select.select(inputs, outputs, inputs)
+            for s in readables:
+                if s is server_socket:
+                    client_socket, client_address = s.accept()
+                    print('Accepted connection from {}:{}'.format(*client_address))
+                    client_socket.setblocking(False)
+                    inputs.append(client_socket)
+                    message_queues[client_socket] = queue.Queue()
                 else:
-                    try:
-                        dec_msg = aes_decrypt(self.Kabc, msg)
-                    except:
-                        dec_msg = msg
-                    # printMsg("==> receive from server aes ", msg)
-                    printMsg("==> receive from server aes_decrypt", dec_msg)
-                if len(self.key_dict) == 3:
-                    key_list = self.key_dict.values()
-                    self.Kabc = bytes_to_base64(combine_hash_values(list(key_list)))[:32]
-                    printMsg("===>self.Kabc", self.Kabc)
+                    data = s.recv(4 * 1024)
+                    if data:
+                        msg = pickle.loads(data)
+                        print('Received data from {}:{}'.format(*s.getpeername()))
+                        printMsg("receive from client <==", msg)
+                        message_queues[s].put(msg)
+                        if s not in outputs:
+                            outputs.append(s)
+                    else:
+                        print('Closed connection from {}:{}'.format(*s.getpeername()))
+                        if s in outputs:
+                            outputs.remove(s)
+                        inputs.remove(s)
+                        s.close()
+                        del message_queues[s]
 
+            for s in writables:
+                try:
+                    next_msg = message_queues[s].get_nowait()
+                except queue.Empty:
+                    outputs.remove(s)
+                else:
+                    self.handle_client(s, next_msg)
+
+            for s in exceptions:
+                print('Handling exception for {}'.format(s.getpeername()))
+                inputs.remove(s)
+                if s in outputs:
+                    outputs.remove(s)
+                s.close()
+                del message_queues[s]
+
+    """
+    :param: conn, data
+    :return: none Process forwarded data. When A, B, C sends A message to other devices A, B, C, 
+             S only forwards the message without parsing it
+    """
+    def handle_message(self, conn, data):
+        result = bytes_to_dict(data.get("message").get("cipher"))
+        content = result.get("content")
+        send_source = content["send_source"]
+        send_to = content["send_to"]
+        send_to_sock = self.sock_dict.get(send_to)
+        if send_to_sock is not None:
+            send_to_sock.send(pickle.dumps(data))
+        else:
+            print(f"Error: Failed to find socket for client {send_to}")
+        print(f" Server S send message from {send_source} to other Client {send_to_sock}")
+        return None
+
+    def handle_client(self, conn, result):
+        print(f"====>{result}")
+        if isinstance(result, dict) == False:
+            conn.sendall(pickle.dumps(result))
+            return
+
+        # Server S load data
+        optional = result["optional"]
+        message = result["message"]
+        try:
+            if optional is not None:
+                self.handle_message(conn, result)
+            else:
+                # The private key of the S server is used to decrypt the client using the rsa decryption algorithm
+                plain_text = bytes_to_dict(rsa_decrypt(self.client_key, message["key"]))
+                client = plain_text["client"]
+
+                # Construct message data that is used to verify digital signatures
+                hash_result = {"hashKey": double_hash(plain_text)}
+                hash_result_s = message["sign"]
+
+                # Obtain the client's public key, which is used to verify the digital signature
+                public_key = self.all_ca_dict.get("public_key").get(f"{client}_CA_public_key")
+
+                # Verify digital signatures. compare True indicates that the verification succeeds,
+                # False indicates that the verification fails
+                compare = RSA_verify(public_key, hash_result, hash_result_s)
+
+                if compare:
+
+                    # If the authentication is successful, the original message is decrypted and the plaintext is obtained
+                    plain_text_res = bytes_to_dict(message["cipher"])
+
+                    # save client info  then Forward to other client
+                    send_source = plain_text_res.get("content").get("send_source")
+
+                    self.sock_dict[send_source] = conn
+                    if plain_text_res["action"] == "login":
+                        self.handle_login(conn, plain_text_res["content"])
+                    else:
+                        print("error")
         except Exception as e:
             print(f"Error: Failed to serialize message: {e}")
+            return None
+
+    """
+    :param: conn, data
+    :return: Processing First login, A, B, S first attempt to communicate with S processing
+             This includes RSA encryption of transmitted messages
+             This includes the source of the digital signature verification number
+    """
+    def handle_login(self, conn, data):
+        ca_name = data["public_ca_name"]
+        ca_value = data["public_ca_value"]
+        sign_value = data["sign_value"]
+        sign_result = RSA_verify(ca_value, ca_value, sign_value)
+        if self.all_public_ca_dict.get(ca_name) == ca_value and sign_result == True:
+            expired_timestamp = int(time.time())
+            reply_dict = {
+                "reply_action": "login_success",
+                "content": {
+                    "expired": expired_timestamp,
+                    "client": ca_name.split("_")[0]
+                }
+            }
+        else:
+            reply_dict = {
+                "reply_action": "login_failed",
+                "content": "please use a correct ca public key"
+            }
+        reply_data = pickle.dumps(reply_dict)
+        conn.sendall(reply_data)
         return None
 
-    def start_client(self):
-        print("====" * 10)
-        print(f"client: {self.client} starting")
-        print(f"client {self.client} public_key is {self.client_public_key}")
-        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_sock.connect((self.host, self.port))
 
-        # Gets the current date and time
-        now = datetime.datetime.now()
-
-        # Format the output date-time string
-        now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-
-        cmd = """The commands are:
-        login       Obtain login credentials
-        exchange    exchange CLIENT_NAME Na, Nb, Nc random number
-        send_to     send_to CLIENT_NAME msg\n"""
-
-        print(cmd)
-
-        while True:
-            readable, writable, exceptional = select.select([client_sock, sys.stdin], [], [])
-            for sock in readable:
-                if sock is client_sock:
-                    data = sock.recv(1024*4)
-                    if not data:
-                        print("Disconnected from server")
-                        sys.exit()
-                    else:
-                        try:
-                            self.receive_message(sock, pickle.loads(data))
-                        except Exception as e:
-                            print(e)
-                else:
-                    message = sys.stdin.readline().strip()
-                    if message == "login":
-                        self.login(client_sock)
-                    elif "exchange" in message:
-                        _, cli_name = message.split(" ")
-                        self.exchange_message(client_sock, cli_name)
-                    elif "send_to" in message:
-                        _, cli_name, msg = message.split(" ")
-                        self.handle_send_msg(client_sock, cli_name, msg, "")
-                    else:
-                        client_sock.send(pickle.dumps(message))
+if __name__ == '__main__':
+    server = ChatServer()
+    server.server()
